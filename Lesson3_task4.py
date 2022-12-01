218eda5405bda2db10bbe031e85c5bb93ade3193
dashapallna <119065748+dashapallna@users.noreply.github.com>
2022-12-02 00:13:24 +0300

    Update Lesson3_task4.py
    не придумала, как без if/else сделать. Это решение тоже не потестила, сломала пайчарм(
    ноль не учитывала

--- Lesson3_task4.py
+++ Lesson3_task4.py
@@ -4,11 +4,34 @@ number_1 = int(input('Введите первое число: '))
 number_2 = int(input('Введите второе число: '))
 number_3 = int(input('Введите третье число: '))
 
-number_1 = bool(number_1>0)
-number_2 = bool(number_2>0)
-number_3 = (number_3>0)
+positive = 0
+negative = 0
 
-print(f'Число {number_1}')
+if number_1 > 0
+    positive += 1
+else:
+    negative += 1
+if number_2 > 0
+    positive += 1
+else:
+    negative += 1
+if number_3 > 0
+    positive += 1
+else:
+    negative += 1
+
+print('Положительных чисел: ', positive, '\nОтрицательных чисел: ', negative)
+
+
+# number_1 = int(input('Введите первое число: '))
+# number_2 = int(input('Введите второе число: '))
+# number_3 = int(input('Введите третье число: '))
+
+# number_1 = bool(number_1>0)
+# number_2 = bool(number_2>0)
+# number_3 = (number_3>0)
+
+# print(f'Число {number_1}')
 
 
 
