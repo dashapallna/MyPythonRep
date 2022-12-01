76266d49eced0e8f336f218a0933f0efd35ef735
dashapallna <dasha.davidovich23@gmail.com>
2022-12-01 23:24:44 +0300

    less3

---
+++ Lesson3_task4.py
@@ -0,0 +1,16 @@
+#Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+
+number_1 = int(input('Введите первое число: '))
+number_2 = int(input('Введите второе число: '))
+number_3 = int(input('Введите третье число: '))
+
+number_1 = bool(number_1>0)
+number_2 = bool(number_2>0)
+number_3 = (number_3>0)
+
+print(f'Число {number_1}')
+
+
+
+# numbers = input('Введите три числа через запятую: ')
+# numbers = numbers.split(',')
